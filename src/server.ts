916c27977a375960281340a6f28f131cbfb24238
Felipe Rodrigo de Souza Gonçalves <f_g@outlook.com.br>
2021-02-26 09:59:53 -0300

    second commit

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,10 @@
-import express, { request, response } from 'express';
+import 'reflect-metadata';
+import express from 'express'; //coloquei anteriormente "import express {request, response} from 'express; NÃO SEI O MOTIVO (autocompletar às vezes atrapalha)"
+import "./database";
+import {router} from "./routes";
 
 const app = express();
-
-/**
- * GET=> Buscar
- * POST=> Salvar
- * PUT=> Alterar
- * DELETE=>Deletar
- * PATCH=>Alteração específica
- * 
- */
-
-
- //http://localhost:3333/users
-
-app.get("/", (request, response) =>{
-    //return response.send("Hello World - NLW04");
-    return response.json({message: "Hello World - NLW04"});
-});
-
-//1º param => Rota(Recurso API)
-//2º param => request,response
-//Utilizar o aplicativo "Insomnia" para simular as requisições dos clientes
-app.post("",(request,response)=>{
-    return response.json({message: "Os dados foram salvos com sucesso!"});
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333,() => console.log("Server is running!"));
